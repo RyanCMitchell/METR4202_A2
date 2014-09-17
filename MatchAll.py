f6c55c08a33b87e617fc6c2be53e0345f9e5bd11
John Pidgeon <john.pidgeon@uqconnect.edu.au>
2014-09-17 20:28:28 +1000

    Working sexily but there is an error with findKeyPoint length of lists under a certain dist threshold.

--- MatchAll.py
+++ MatchAll.py
@@ -1,6 +1,51 @@
 from os.path import isfile, join
 from os import listdir
+import cv2
+import numpy as np
+import itertools
+import sys
+from MatchingFunctions import findKeyPoints, drawKeyPoints, match, findKeyPointsDist, drawImageMappedPoints
 
-#Prepare a list of test images
-mypath = "Test/"
-images = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
+#Clear all cv windows
+cv2.destroyAllWindows()
+
+#Prepare a list of different training images
+pathlarge = "TrainingImages/LargeCup/"
+pathmedium = "TrainingImages/MediumCup/"
+pathsmall = "TrainingImages/SmallCup/"
+pathtest = "TestImages"
+
+largecups = [ f for f in listdir(pathlarge) if isfile(join(pathlarge,f)) and f[0]<>"."]
+mediumcups = [ f for f in listdir(pathmedium) if isfile(join(pathmedium,f)) and f[0]<>"."]
+smallcups = [ f for f in listdir(pathsmall) if isfile(join(pathsmall,f)) and f[0]<>"."]
+testimages = [ f for f in listdir(pathtest) if isfile(join(pathtest,f)) and f[0]<>"."]
+
+img = cv2.imread(str(pathtest+"/"+testimages[3]))
+maxdist = 25
+
+KeyPointsTotalList = []
+DistsTotalList = []
+
+for i in largecups+mediumcups+smallcups:
+    if i in largecups:
+        temp = cv2.imread(str(pathlarge+"/"+i))
+    elif i in mediumcups:
+        temp = cv2.imread(str(pathmedium+"/"+i))
+        
+    elif i in smallcups:
+        temp = cv2.imread(str(pathsmall+"/"+i))
+    print i
+    print len(findKeyPointsDist(img,temp,maxdist)[0])
+    print len(findKeyPointsDist(img,temp,maxdist)[1])
+
+    KeyPointsTotalList += (findKeyPointsDist(img,temp,maxdist))[0]
+    DistsTotalList += (findKeyPointsDist(img,temp,maxdist))[1]
+    
+indices = range(len(DistsTotalList))
+indices.sort(key=lambda i: DistsTotalList[i])
+DistsTotalList = [DistsTotalList[i] for i in indices]
+KeyPointsTotalList = [KeyPointsTotalList[i] for i in indices]
+
+
+drawImageMappedPoints(img, KeyPointsTotalList)
+
--- MatchingFunctions.py
+++ MatchingFunctions.py
@@ -1,9 +1,9 @@
-import cv2
-import numpy as np
-import itertools
-import sys
+def findKeyPoints(img, template, maxdist=200):
+    import cv2
+    import numpy as np
+    import itertools
+    import sys
 
-def findKeyPoints(img, template, distance=200):
     detector = cv2.FeatureDetector_create("SIFT")
     descriptor = cv2.DescriptorExtractor_create("SIFT")
 
@@ -14,6 +14,7 @@ def findKeyPoints(img, template, distance=200):
     tkp, td = descriptor.compute(template, tkp)
 
     flann_params = dict(algorithm=1, trees=4)
+    
     flann = cv2.flann_Index(sd, flann_params)
     idx, dist = flann.knnSearch(td, 1, params={})
     del flann
@@ -27,7 +28,7 @@ def findKeyPoints(img, template, distance=200):
     idx = [idx[i] for i in indices]
     skp_final = []
     for i, dis in itertools.izip(idx, dist):
-        if dis < distance:
+        if dis < maxdist:
             skp_final.append(skp[i])
 
     flann = cv2.flann_Index(td, flann_params)
@@ -43,12 +44,55 @@ def findKeyPoints(img, template, distance=200):
     idx = [idx[i] for i in indices]
     tkp_final = []
     for i, dis in itertools.izip(idx, dist):
-        if dis < distance:
+        if dis < maxdist:
             tkp_final.append(tkp[i])
 
     return skp_final, tkp_final
 
+def findKeyPointsDist(img, template, maxdist=200):
+    import cv2
+    import numpy as np
+    import itertools
+    import sys
+
+    detector = cv2.FeatureDetector_create("SIFT")
+    descriptor = cv2.DescriptorExtractor_create("SIFT")
+
+    skp = detector.detect(img)
+    skp, sd = descriptor.compute(img, skp)
+
+    tkp = detector.detect(template)
+    tkp, td = descriptor.compute(template, tkp)
+
+    flann_params = dict(algorithm=1, trees=4)
+    
+    flann = cv2.flann_Index(sd, flann_params)
+    idx, dist = flann.knnSearch(td, 1, params={})
+    del flann
+
+    dist = dist[:,0]/2500.0
+    dist = dist.reshape(-1,).tolist()
+    idx = idx.reshape(-1).tolist()
+    indices = range(len(dist))
+    indices.sort(key=lambda i: dist[i])
+    dist = [dist[i] for i in indices]
+    idx = [idx[i] for i in indices]
+    skp_final = []
+    skp_final_dist = []
+    for i, dis in itertools.izip(idx, dist):
+        if dis < maxdist:
+            skp_final.append(skp[i])
+            skp_final_dist.append(dis)
+
+    return skp_final, skp_final_dist
+
+
 def drawKeyPoints(img, template, skp, tkp, num=-1):
+    import cv2
+    import numpy as np
+    import itertools
+    import sys
+
     h1, w1 = img.shape[:2]
     h2, w2 = template.shape[:2]
     nWidth = w1+w2
@@ -67,18 +111,31 @@ def drawKeyPoints(img, template, skp, tkp, num=-1):
         cv2.line(newimg, pt_a, pt_b, (255, 0, 0))
     return newimg
 
+def drawImageMappedPoints(img, skptotal, num=-1):
+    import cv2
+    import numpy as np
+    import itertools
+    import sys
 
-def match(img, template):
-    img = cv2.imread(img)
-    temp = cv2.imread(template)
-    try:
-    	dist = int(sys.argv[3])
-    except IndexError:
-    	dist = 200
-    try:
-    	num = int(sys.argv[4])
-    except IndexError:
-    	num = -1
+    maxlen = len(skptotal)
+    if num < 0 or num > maxlen:
+        num = maxlen
+    for i in range(num):
+        pt_b = (int(skptotal[i].pt[0]), int(skptotal[i].pt[1]))
+        cv2.circle(img, pt_b, 5, (255, 0, 0))
+    cv2.imshow("image", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def match(img, temp, dist = 200, num = -1):
+    import cv2
+    import numpy as np
+    import itertools
+    import sys
+
+    #img = cv2.imread(img)
+    #temp = cv2.imread(template)
+    
     skp, tkp = findKeyPoints(img, temp, dist)
     newimg = drawKeyPoints(img, temp, skp, tkp, num)
     cv2.imshow("image", newimg)

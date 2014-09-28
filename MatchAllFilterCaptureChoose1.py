82a0810543d18191ceb4a35ad765db80f8ea2896
John Pidgeon <john.pidgeon@uqconnect.edu.au>
2014-09-28 17:52:02 +1000

    Started Cup classification

--- MatchAllFilterCaptureChoose1.py
+++ MatchAllFilterCaptureChoose1.py
@@ -2,7 +2,7 @@ from os.path import isfile, join
 from os import listdir
 import numpy as np
 import freenect, itertools, sys, time, cv2
-from MatchingFunctions import findKeyPoints, drawKeyPoints, match, findKeyPointsDist, drawImageMappedPoints, saveImageMappedPoints, MatchAllCapture, Cluster
+from MatchingFunctions import findKeyPoints, drawKeyPoints, match, findKeyPointsDist, drawImageMappedPoints, saveImageMappedPoints, MatchAllCapture, Cluster, fit_ellipses
 from matplotlib import pyplot as plt
 from math import sqrt
 from convertDepth import convertToWorldCoords
@@ -57,10 +57,12 @@ def MatchAllCluster(save, maxdist=200, filtparam=2.0):
     for j in xrange(groups):
         segregatedF[j] = np.array(segregatedF[j])
 
-    #remove clusters that are 3 points or smaller
+    #remove clusters that are >= 3 points or all superimposed
     for i in xrange(groups):
         if len(segregatedF[i]) <= 5 or np.isnan(np.std(segregatedF[j])):
             print "HOLY FUCKING SHIT"
+            cv2.imshow("Cups Stream", img)
+            return
 
     # Create a centriod depth list
     FinalCenters = []
@@ -71,9 +73,57 @@ def MatchAllCluster(save, maxdist=200, filtparam=2.0):
     FinalCentersWC = convertToWorldCoords(FinalCenters)
     
     segregated = segregatedF
-
-    # Draw the groups
+    FC = FinalCenters
     colourList=[(0, 255, 0), (255, 0, 0), (0, 0, 255), (0, 255, 255), (255, 255, 0), (255, 0, 255)]
+
+    # Seperate the top of each cup in pixel space
+    depthimg = img.copy()
+
+    # Start Cup Classification loop
+    for j in xrange(groups):
+        # Choose pixel area likley to contain a cup
+        w = -0.08811*FC[j][2]+103.0837
+        h = -0.13216*FC[j][2]+154.6256
+        cup1 = depthimg[(FC[j][1]-h):(FC[j][1]), (FC[j][0]-w):(FC[j][0]+w)]
+
+        # Determine the bouding rectangle of the largest contour in that area
+        gray = cv2.cvtColor(cup1,cv2.COLOR_BGR2GRAY)
+        blur = cv2.GaussianBlur(gray,(5,5),0)
+        thresh = 240
+        global contours
+        edges = cv2.Canny(blur,thresh,thresh*2)
+        contours,hierarchy = cv2.findContours(edges,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+        cnt = max(contours, key=len)
+        x,y,w1,h1 = cv2.boundingRect(cnt)
+
+        # Use brounding rectangle size to determine cup type
+        s1 = [(FC[j][0]-w)+x,(FC[j][1]-h)+ y,FC[j][2]]
+        s2 = [(FC[j][0]-w)+x+w1,(FC[j][1]-h)+ y,FC[j][2]]
+        top = [s1,s2]
+        topWorld = convertToWorldCoords(top)
+        CupTopWidth = topWorld[1][0]-topWorld[0][0]
+
+        if CupTopWidth > 100:
+            cupType = "Not a Cup"
+        elif CupTopWidth > 84.5:
+            cupType = "Large"
+        elif CupTopWidth > 71:
+            cupType = "Medium"
+        elif CupTopWidth > 50:
+            cupType = "Small"
+        else:
+            cupType = "Not a Cup"
+
+        FinalCentersWC[j].append(cupType)
+
+        #Draw the top of the bounding rectangle
+        cv2.line(img,(int(round(s1[0],0)),int(round(s1[1],0))),(int(round(s2[0],0)),int(round(s2[1],0))),colourList[j])
+##        new_cnt = [[x[0][0],x[0][1]] for x in cnt]
+##        print new_cnt
+                
+        cv2.drawContours(img,[cnt],0,colourList[j],2)
+    
+    # Draw the groups
     for j in xrange(groups):
         centerst = tuple(np.array(centers[j])+np.array([0,50]))
         cv2.putText(img,str(FinalCentersWC[j]), centerst, cv2.FONT_HERSHEY_SIMPLEX, 0.3, colourList[j])
@@ -84,15 +134,34 @@ def MatchAllCluster(save, maxdist=200, filtparam=2.0):
             cv2.circle(img, pt_a, 3, colourList[j])
         rpt1 = tuple(segregated[j].min(axis=0))
         rpt2 = tuple(segregated[j].max(axis=0))
-        cv2.rectangle(img, rpt1, rpt2, colourList[j])
+        #cv2.rectangle(img, rpt1, rpt2, colourList[j])
     
     if save == 1:
         cv2.imwrite('ProcessedImages/ProcessedCluster'+str(ImageNo)+'.jpg', img)
 
-    #print FinalCenters
-    #print FinalCentersWC
+    print FinalCenters
+    print FinalCentersWC
+    
+    
+    cv2.imshow("Cups Stream", img)
 
-    return img
+    
+    
+    """
+    depthFindCup1 = img.copy()
+    depthFindCup1.fill(0)
+    dc = FinalCentersWC[0][2]
+    for i in xrange(depth.shape[0]):
+        for j in xrange(depth.shape[1]):
+            if dc-20<depth[i,j]<dc+70:
+                depthFindCup1[i,j] = [255,255,255]
+    gray = cv2.cvtColor(depthFindCup1, cv2.COLOR_BGR2GRAY)
+    gray = cv2.bilateralFilter(gray, 11, 17, 17)
+    edged = cv2.Canny(gray, 30, 200)
+    (cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(img, cnts, -1, (0, 255, 0), 1)
+    """
+    
     
     """
     # Black out 0 depth
@@ -112,7 +181,7 @@ if __name__== '__main__':
     
     cv2.destroyAllWindows()
     while 1<2:
-        cv2.imshow("Cups Stream", MatchAllCluster(0,80,2))
+        MatchAllCluster(0,80,2)
         cv2.waitKey(10)
 
     
--- MatchingFunctions.py
+++ MatchingFunctions.py
@@ -299,6 +299,44 @@ def Cluster(Z, groups = 3):
 
     return segregated, centers, distFromCenter, distFromCenterAve
 
+def rgb_to_gray(img):
+    return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+def do_threshold(image, threshold = 170):
+    (thresh, im_bw) = cv2.threshold(image, threshold, 255, cv2.THRESH_BINARY)
+    return (thresh, im_bw)
+
+def fit_ellipses(img):
+    import cv2
+    
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    img = cv2.medianBlur(img, 5)
+    #ret, thresh = cv2.threshold(img, 169, 255, 0)
+    thresh = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+    contours, hierarchy = cv2.findContours(thresh, 1, 2)
+
+    #print contours
+    cntLengths = [len(i) for i in contours]
+    cntMax = cntLengths.index(max(cntLengths))
+
+    for i in range(len(contours)):
+        if 10 < len(contours[i]) < 1500:
+            cnt = contours[i]
+            M = cv2.moments(cnt)
+            #print M
+
+            cx = int(M['m10']/M['m00'])
+            cy = int(M['m01']/M['m00'])
+            print "cx: ", cx
+            print "cy: ", cy
+             
+            ellipse = cv2.fitEllipse(cnt)
+            cv2.ellipse(img, ellipse, (0, 255, 0), 2)
+
+    cv2.imshow('img', img)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
     
 if __name__== '__main__':
     MatchAllCapture(0)
